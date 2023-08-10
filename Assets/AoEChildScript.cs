a626f9f05f54501421eb7c3dd8127dfe04945056
ChoiDDoSik <mgs7987@gmail.com>
2023-08-10 14:40:30 +0900

    AoE speed normalize attempt

--- Assets/AoEChildScript.cs
+++ Assets/AoEChildScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50baad38634bac67d6630b34f209ed295ed3c2463b5dc9f701c39a4eba9caab0
-size 1550
+oid sha256:95abd0a9ec6ec69a306927aa3d19ab904d9b61e0a6d9d8ca0801f30b3eae34c5
+size 1524
--- Assets/PlayerClass.cs
+++ Assets/PlayerClass.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6fcadff42a0068ca6145d16b78be0af09fb7d9f0d2c4df4845deee9a294f6461
-size 739
+oid sha256:5b754e1df2ff163b07949415330ab368abaac992bdb6f832853c157496526601
+size 788
